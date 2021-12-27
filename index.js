36e060ad7e4651fe2372977ca4d8779bb4bbc86e
layliu <layliu@microsoft.com>
2021-12-27 13:28:15 +0800

    Update workflow for githubactiontesticlksxcs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("iclksxcs" + process.versions.node);
+    response.end("iclksxcs_2" + process.versions.node);
 });
 
 const port = process.env.PORT || 1337;
